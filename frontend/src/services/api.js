71874578a7d88d3df81492320cee9ece5fed3fad
nani3467 <lnagaraju274@gmail.com>
2026-06-05 11:24:30 +0530

    edited req

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,11 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://lifepulse-2.onrender.com/api',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  timeout: 15000,
 })
+
 
 // Attach JWT token
 api.interceptors.request.use(
